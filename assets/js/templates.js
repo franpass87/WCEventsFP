2ddfc8d9492d652f1d85694b60b962a14bf13156
franpass87 <francesco.passeri@gmail.com>
2025-08-18 08:40:02 +0200

    Create templates.js

---
+++ assets/js/templates.js
@@ -0,0 +1,6 @@
+(function($){
+  $(function(){
+    // Qui per ora non serve JS: le card sono statiche.
+    // Lasciamo il file per estensioni future (es. lazy-load, filtri client, ecc.).
+  });
+})(jQuery);
